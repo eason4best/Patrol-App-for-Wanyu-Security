e0656d585233ddb708ce3ca9746df4bf59b2a12e
eason4best <eason4best@gmail.com>
2022-09-29 16:48:44 +0800

    Minor Improve

--- lib/widget/upload_onboard_document_widget.dart
+++ lib/widget/upload_onboard_document_widget.dart
@@ -20,7 +20,7 @@ class UploadOnboardDocumentWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: onTap,
+      onTap: image == null ? onTap : null,
       child: AspectRatio(
         aspectRatio: documentType.aspectRatio() ?? 1,
         child: Card(
@@ -31,13 +31,30 @@ class UploadOnboardDocumentWidget extends StatelessWidget {
           margin: const EdgeInsets.all(0),
           child: Center(
             child: image != null && image!.isNotEmpty
-                ? Container(
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: MemoryImage(image!),
-                        fit: BoxFit.cover,
+                ? Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: MemoryImage(image!),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
                       ),
-                    ),
+                      ElevatedButton(
+                        onPressed: onTap,
+                        style: ButtonStyle(
+                          elevation: MaterialStateProperty.all(0.0),
+                          shape: MaterialStateProperty.all(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                          ),
+                        ),
+                        child: const Text('重拍'),
+                      ),
+                    ],
                   )
                 : Container(
                     margin: const EdgeInsets.symmetric(horizontal: 32),
